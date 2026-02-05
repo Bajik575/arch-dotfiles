1b9de9173b85cab7ae7c7feeb1c951b082645e53
Bajik575 <ivanabasin2@gmail.com>
2026-02-05 22:23:15 +0300

    update foot config and add fish vars and aliases

--- fish/config.fish
+++ fish/config.fish
@@ -1,4 +1,23 @@
 if status is-interactive
-# Commands to run in interactive sessions can go here
+    # vars
+    set -gx EDITOR "micro"
+    set -gx AUR_INSTALLER "paru"
+    
+    # aliases
+    # cfg
+    alias _hypr "$EDITOR ~/.config/hypr/hyprland.conf"
+    alias _fish "$EDITOR ~/.config/fish/config.fish"
+    
+    # shortcuts
+    alias pcm "sudo pacman -S --noconfirm"
+    alias aur "$AUR_INSTALLER -S --noconfirm"
+    alias sysd "sudo systemctl"
+    alias b "btop"
+    
+    # system control
+    alias hypr-logout "hyprctl dispatch exit"
+    alias off "shutdown now"
+    alias suspend "systemctl suspend"
+    alias hibernate "systemctl hibernate"
 end
 set -g fish_greeting ""
