47fd34643333d867a684c5cc6a371ecd0c09c417
Bajik575 <ivanabasin2@gmail.com>
2026-02-11 12:47:29 +0300

    Big update for hyprland(rofi, clipboard with screnshots, waybar)

---
+++ hypr/scripts/power-menu.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+CHOICE=$(echo -e "Poweroff\nReboot\nSuspend\nLogout\nLock" | rofi -dmenu -p "menu" -i)
+
+case "$CHOICE" in
+    "Poweroff") shutdown now ;;
+    "Reboot") reboot ;;
+    "Suspend") systemctl suspend ;;
+    "Logout") hyprctl dispatch exit ;;
+    "Lock") hyprlock ;;
+esac
